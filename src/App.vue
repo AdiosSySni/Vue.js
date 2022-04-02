b7c72653da98cd0a4eb461034f080c124cf6cffd
AdiosSyS <62465250+AdiosSySni@users.noreply.github.com>
2022-04-02 13:07:27 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import router from './router'
+</script>
+<template>
+<header>
+    <div class="header-nav container">
+        <div class="logo">
+
+        </div>
+        <router-link
+            v-for="route in router.getRoutes().filter((item)=>(item.path !== router.currentRoute.value.path || item.path == '/dashboard') )"
+            :to="route.path"
+            :key="route.name">
+            {{route.name}}
+        </router-link>
+    </div>
+    
+    <router-view></router-view>
+</header>
+</template>
+
+<style lang="sass">
+    *
+        margin: 0
+        padding: 0
+
+    .container 
+        padding: 0 400px
+        // max-width: 1200px
+        // margin: 0 auto
+
+    .logo 
+        background: brown
+        width: 40px
+        
+    .header-nav
+        display: flex
+        justify-content: space-between
+        a
+            color: black
+            text-decoration: none
+            font-size: .5vw 
+        
+    
+</style>
---
+++ src/pages/Account.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1>Account</h1>
+</template>
+
+<style scoped lang="sass">
+    h1 
+        font-size:36px
+        color: red
+</style>
